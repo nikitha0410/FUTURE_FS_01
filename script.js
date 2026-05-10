8d43aee3dde044dae2af0f30bdfbf83cfa7db1e1
Nikitha <nikithaj0410@gmail.com>
2026-05-10 12:24:25 +0530

    Portfolio Website

---
+++ script.js
@@ -0,0 +1,33 @@
+const text = [
+  "CSE Student",
+  "Web Developer",
+  "Python Learner",
+  "Tech Enthusiast"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type(){
+
+  if(count === text.length){
+    count = 0;
+  }
+
+  currentText = text[count];
+  letter = currentText.slice(0, ++index);
+
+  document.getElementById("typing").textContent = letter;
+
+  if(letter.length === currentText.length){
+    count++;
+    index = 0;
+    setTimeout(type,1000);
+  }
+  else{
+    setTimeout(type,120);
+  }
+
+})();
